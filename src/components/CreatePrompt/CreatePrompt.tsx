993e1c173d414d4f41d9c610f46ccfd809885377
Romal-2303 <angryrocket789@gmail.com>
2025-05-21 19:07:09 +0530

    Developed create page

--- src/components/CreatePrompt/CreatePrompt.tsx
+++ src/components/CreatePrompt/CreatePrompt.tsx
@@ -3,14 +3,22 @@ import Dropdown from "../Dropdown/Dropdown";
 
 const CreatePrompt = () => {
   return (
-    <div className="flex flex-col p-4  w-[200px] border border-red-500">
-      <div className="flex flex-col">
+    <div className="flex flex-col p-4">
+      <div className="flex flex-col  w-[300px]">
         <label className="text-[14px]">Enter Prompt</label>
-        <input className="border border-black rounded mt-2" />
+        <textarea className="border border-black rounded mt-2  pl-2 pt-2 h-[120px] text-[12px]" />
       </div>
-      <div className="flex gap-2 item-center mt-8">
-        <p>Select Tone:</p>
-        <Dropdown />
+      <div className="flex gap-2 item-center mt-8  w-[300px]">
+        <Dropdown placeholder="Select Tone" optionsArr={["Formal", "Casual"]} />
+      </div>
+
+      <div className="mt-8 flex gap-4  w-[300px]">
+        <button className="text-[14px] flex-1 px-3 py-1 rounded cursor-pointer bg-blue-500 text-white">
+          Generate Prompt
+        </button>
+        <button className="text-[14px] flex-1 px-3 py-1 rounded cursor-pointer border border-black hover:bg-black/10">
+          Save Template
+        </button>
       </div>
     </div>
   );
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,26 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { DropdownMenu } from "radix-ui";
 import {
   HamburgerMenuIcon,
   DotFilledIcon,
   CheckIcon,
-  ChevronRightIcon,
+  ChevronDownIcon,
 } from "@radix-ui/react-icons";
 
-const Dropdown = () => {
+interface DropdownProps {
+  placeholder: string;
+  optionsArr: string[];
+}
+
+const Dropdown = ({ placeholder, optionsArr }: DropdownProps) => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const handleSelect = (value: string) => {
+    setSelectedOption(value);
+  };
+
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <button className="IconButton" aria-label="Customise options">
-          <HamburgerMenuIcon />
-        </button>
+      <DropdownMenu.Trigger className="flex justify-between items-center px-4 py-1 rounded border border-black text-[14px] w-full">
+        <p>{selectedOption || placeholder}</p> <ChevronDownIcon />
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
-          <DropdownMenu.Item className="DropdownMenuItem">
-            New Tab <div className="RightSlot">⌘+T</div>
-          </DropdownMenu.Item>
+        <DropdownMenu.Content
+          className="min-w-[220px] w-full rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
+          sideOffset={5}
+        >
+          {optionsArr.map((option) => (
+            <DropdownMenu.Item
+              className="group relative flex h-[40px] select-none items-center rounded-[3px] pl-[25px] pr-[5px] text-[13px] leading-none text-violet-500 outline-none cursor-pointer"
+              onSelect={() => handleSelect(option)}
+            >
+              {option}
+            </DropdownMenu.Item>
+          ))}
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
